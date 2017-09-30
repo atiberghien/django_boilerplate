6d8cf2757152aa6883db59f4d0260350d0f978f3
Alban Tiberghien <alban.tiberghien@gmail.com>
2017-09-30 09:06:59 +0200

    Initial import

---
+++ project_name/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="homepage.html"), name="home")
+
+]
